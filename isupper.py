7b48aac31198efc04f2ff03e1b0e4ba2586ea286
unknown <yisehakalelign7077@gmail.com>
2025-01-10 02:27:19 -0600

    string methods

--- isupper.py
+++ isupper.py
@@ -1,2 +1,2 @@
 age = "twenty one" ## this string is specfied by the variable called age
-print(age.upper()) # this will print the string by changing it to upper case
+print(age.upper()) # this will print the string by changing it to upper caseg
