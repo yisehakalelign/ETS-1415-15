71714b63c8019ff5359c8ec54d542b0b403bb418
unknown <yisehakalelign7077@gmail.com>
2025-01-10 02:07:38 -0600

    string methods

---
+++ main.py
@@ -0,0 +1,2 @@
+name = "YISEHAK" # specified a string in na,e vareable
+print(name.lower()) # this will print the specified string by changing thr letters in to small case
---
+++ title.py
@@ -0,0 +1,2 @@
+object = "chair & table" # this string is specified by variable chair and table
+print(object.title()) # i expect this string to print "Chair & Table"
