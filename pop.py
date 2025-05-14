78e0e7b7938ecc4f92947475dd05e632018ccf00
yisehakalelign <yisehakalelign7077@gmail.com>
2025-05-14 13:05:04 -0600

    all list_methods

--- pop.py
+++ pop.py
@@ -1,3 +1,3 @@
 vege = ['carrot', 'tomatto', 'potato', 'onion'] # a variable is specified in a list
-vege.pop() # a list metod called pop is used
+vege.pop() # a list metod called pop is used.
 print(vege) # the last element in the list will be removed
