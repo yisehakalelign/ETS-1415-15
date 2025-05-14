7de4e409d8757f9f5afb474b3791cbd9de79aef9
yisehakalelign <yisehakalelign7077@gmail.com>
2025-05-14 13:03:21 -0600

    all list_methods

--- len.py
+++ len.py
@@ -1,3 +1,3 @@
 grades = ['A', 'B', 'C', 'D', 'F'] # a variable is specified in a list
-length = len(grades) # a list method called len is used
+length = len(grades) # a list method called len is used.
 print(length) # this will print "5"
