280751722878d9567dab62f1de84ab7730fb8a90
yisehakalelign <yisehakalelign7077@gmail.com>
2025-05-14 12:59:43 -0600

    all list_methods

--- insert.py
+++ insert.py
@@ -1,3 +1,3 @@
 feildgames = ['football','basketball', 'vollyball'] # variables are listed in an index
-feildgames.insert(0,'handball') # a list method called insert is used to add an element to the list
+feildgames.insert(0,'handball') # a list method called insert is used to add an element to the list.
 print(feildgames) "handball is expected to be printed in the list"
