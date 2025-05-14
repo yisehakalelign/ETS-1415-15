0eeeda67ccdf5dcad004db4ebf8f3ff59663bd8e
yisehakalelign <yisehakalelign7077@gmail.com>
2025-05-14 12:54:56 -0600

    all list_methods

--- append.py
+++ append.py
@@ -1,3 +1,3 @@
 materials = ['mouse', 'pc', 'keybord'] # variable called materials is listed
-materials.append("cell phone") # a list method called append is used to add another list
+materials.append("cell phone") # a list method called append is used to add another list.
 print(materials) # cell phone is expected to be printed in the list aswell.
